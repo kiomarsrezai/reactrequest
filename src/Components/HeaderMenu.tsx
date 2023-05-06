a007bf09658e871d86089c69b67a441de999e08b
kio <127694794+kiomarsrezai@users.noreply.github.com>
2023-05-06 22:56:12 +0430

    update packages

---
+++ src/Components/HeaderMenu.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+
+function HeaderMenu(){
+
+    return (
+        <div className="container">
+            <li>
+                <ul>kia</ul>
+                <ul>kasra</ul>
+            </li>
+        </div>
+    )
+}
+
+export default HeaderMenu;
